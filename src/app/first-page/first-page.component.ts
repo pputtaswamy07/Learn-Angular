dad069e4d2673906fb3ba83b3f032a5a90516295
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-14 11:52:07 +0200

    create firstpage component

---
+++ src/app/first-page/first-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-page.component.html',
+  styleUrl: './first-page.component.css'
+})
+export class FirstPageComponent {
+
+}
