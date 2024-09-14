1c9746dbe441a7ad77aa6ba7f98bc3eb14b3c2b0
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-14 11:51:41 +0200

    basic setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FirstPageComponent } from './first-page/first-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: FirstPageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
