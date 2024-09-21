6911fc47e9b57ff1741bb11af78f628b206de5f6
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-21 13:31:46 +0200

    update create-project route link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstPageComponent],
+  imports: [RouterOutlet, FirstPageComponent, CreateProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
-import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
-export const routes: Routes = [{ path: '', component: FirstPageComponent }];
+export const routes: Routes = [
+  { path: '', component: FirstPageComponent, pathMatch: 'full' },
+  { path: 'firstPage', component: FirstPageComponent },
+  { path: 'createProject', component: CreateProjectComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), FormsModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
