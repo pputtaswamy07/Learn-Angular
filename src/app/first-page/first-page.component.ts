7c5ca9016b45000af9bcaad49cbcbec0f0cc59e8
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-21 13:52:19 +0200

    button with click function to route to create-project

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-first-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.css', '../../styles.css'],
 })
