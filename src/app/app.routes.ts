667e5a29edb31356631fc5c70c4bf279204c899d
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-22 22:07:36 +0200

    add route to introduction component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 export const routes: Routes = [
   { path: '', component: FirstPageComponent, pathMatch: 'full' },
   { path: 'firstPage', component: FirstPageComponent },
   { path: 'createProject', component: CreateProjectComponent },
+
+  { path: 'introduction', component: IntroductionComponent },
 ];
 
 @NgModule({
