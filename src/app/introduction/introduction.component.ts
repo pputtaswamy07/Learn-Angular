9c10df23d4ad0bfaee7f29172d122c7c182b24e0
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-09-22 22:07:11 +0200

    create introduction component

---
+++ src/app/introduction/introduction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introduction',
+  standalone: true,
+  imports: [],
+  templateUrl: './introduction.component.html',
+  styleUrls: ['./introduction.component.css', '../../styles.css'],
+})
+export class IntroductionComponent {}
